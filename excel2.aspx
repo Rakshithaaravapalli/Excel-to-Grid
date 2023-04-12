d5438f6e99b4b9db37802872b6b51bd5d977049b
APTOL301956 <APTOL301956@APOLBO03-176>
2023-04-12 11:49:56 +0530

    Add project files.

---
+++ excel2.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="excel2.aspx.cs" Inherits="excel.excel2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 181px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+       Import Excel File:    
+        <asp:FileUpload ID="FileUploadToServer" width="300px" runat="server" />    
+        <br />    
+        <br />    
+        <asp:Button ID="btnUpload" runat="server" OnClick="btnUpload_Click" Text="Upload File" ValidationGroup="vg" Style="width:99px" />    
+        <br />    
+        <br />    
+        <asp:Label ID="lblMsg" runat="server" ForeColor="Green" Text=""></asp:Label>    
+        <br />    
+              <asp:GridView ID="GridView1" runat="server" CellPadding="4"  EmptyDataText="No Records Found!" Height="25px" ForeColor="#333333" GridLines="None" CssClass="auto-style1" Width="295px">    
+            <AlternatingRowStyle BackColor="White" ForeColor="#284775" />    
+            <EditRowStyle BackColor="#999999" />    
+            <FooterStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />    
+            <HeaderStyle BackColor="#5D7B9D" Font-Bold="True" ForeColor="White" />    
+            <PagerStyle BackColor="#284775" ForeColor="White" HorizontalAlign="Center" />    
+            <RowStyle BackColor="#F7F6F3" ForeColor="#333333" />    
+            <SelectedRowStyle BackColor="#E2DED6" Font-Bold="True" ForeColor="#333333" />    
+               
+        </asp:GridView>   
+<%--        <asp:GridView ID="GridView1" runat="server"  GridLines="None" EmptyDataText="No Records Found!" Height="25px">    
+            <RowStyle Width="175px" />
+            <EmptyDataRowStyle BackColor="Silver" BorderColor="#999999" BorderStyle="solid" borderwidth="1px" ForeColor="#003300"/>
+            <HeaderStyle BackColor="#6699ff" BorderColor="#333333" BorderStyle="Solid" BorderWidth="1px" VerticalAlign="Top" Width="200px" Wrap="true" />
+        </asp:GridView>--%>    
+        
+    </div>    
+    </form>    
+</body>    
+</html> 
---
+++ excel2.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+using System.Configuration;
+using System.IO;
+
+namespace excel
+{
+    public partial class excel2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void btnUpload_Click(object sender, EventArgs e)
+       {
+
+            //get path from web.config file to upload
+            string FilePath = ConfigurationManager.AppSettings["FilePath"].ToString();
+            string filename = string.Empty;
+
+            //To check whether file is selected or not
+            if (FileUploadToServer.HasFile)
+            {
+                try
+                {
+                    string[] allowdFile = { ".xls", ".xlsx" };
+                    //here we are allowing only excel file so verifying selected file pdf or not
+                    string FileExt = System.IO.Path.GetExtension(FileUploadToServer.PostedFile.FileName);
+                    //To check whether selected file is valid extension or not
+                    bool isValidFile = allowdFile.Contains(FileExt);
+                    if (!isValidFile)
+                    {
+                        lblMsg.ForeColor = System.Drawing.Color.Red;
+                        lblMsg.Text = "Please Upload only Excel";
+                    }
+                    else
+                    {
+                        //Get size of uploaded file, here restricting size of file
+                        int FileSize = FileUploadToServer.PostedFile.ContentLength;
+                        if (FileSize <= 1048576) //which is =1MB
+                        {
+                            //Get file name of selected file
+                            filename = Path.GetFileName(Server.MapPath(FileUploadToServer.FileName));
+                            //save selected file into server location
+                            FileUploadToServer.SaveAs(Server.MapPath(FilePath) + filename);
+                            //Get file path
+                            string filePath = Server.MapPath(FilePath) + filename;
+
+                            //open the connection with excel file based on excel version
+                            OleDbConnection con = null;
+                            if (FileExt == ".xls")
+                            {
+                                con = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + filePath + ";Extended Properties=\"Excel 8.0;HDR=Yes;IMEX=2\"");
+                            }
+                            else if (FileExt == ".xlsx")
+                            {
+                                con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = " + filePath + "; Extended Properties =\"Excel 12.0;HDR=Yes;IMEX=2\"");
+                            }
+
+                            con.Open();
+
+                            //get the list of sheet available in excel sheet
+                            DataTable dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                            //get first sheet name
+                            string getExcelSheetName = dt.Rows[0]["Table_Name"].ToString();
+                            //select rows from first sheet in excel sheet and fill into dataset
+                            OleDbCommand ExcelCommand = new OleDbCommand(@"SELECT * FROM [" + getExcelSheetName + @"]", con);
+                            OleDbDataAdapter ExcelAdapter = new OleDbDataAdapter(ExcelCommand);
+                            DataSet ExcelDataSet = new DataSet();
+                            ExcelAdapter.Fill(ExcelDataSet);
+                            con.Close();
+                            //Bind the dataset into gridview to display excel contents
+                            GridView1.DataSource = ExcelDataSet;
+                            GridView1.DataBind();
+
+                        }
+                        else
+                        {
+                            lblMsg.Text = "Attachment file size should not be greater than 1 MB!";
+                        }
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Handle the exception and display a user-friendly error message
+                    throw new Exception("Error uploading Excel file: " + ex.Message);
+                }
+            }
+        }
+    }
+}
+
+
+
